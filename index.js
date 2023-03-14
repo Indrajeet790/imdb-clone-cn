952f339229fc5646c0fc04eb76da74e4684f286a
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-14 14:36:30 +0530

    code for searcKeyword

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const searchKeyword = document.getElementById("search");
-const suggestionsContainer = document.getElementById("card-container");
-const favMoviesContainer = document.getElementById("fav-movies-container");
-const emptyText = document.getElementById("empty-search-text");
-const showFavorites = document.getElementById("favorites-section");
-const emptyFavText = document.getElementById("empty-fav-text");
+(function () {
+  const searchKeyword = document.getElementById("search");
+  const suggestionsContainer = document.getElementById("card-container");
+  const favMoviesContainer = document.getElementById("fav-movies-container");
+  const emptyText = document.getElementById("empty-search-text");
+  const showFavorites = document.getElementById("favorites-section");
+  const emptyFavText = document.getElementById("empty-fav-text");
+
+  addToFavDOM();
+  showEmptyText();
+  let suggestionList = [];
+  let favMovieArray = [];
+
+  searchKeyword.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+      event.preventDefault();
+    }
+  });
+})();
