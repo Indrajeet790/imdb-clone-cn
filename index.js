70374a2eb583d7f130912d249e52c82d61da5ad7
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-14 14:33:05 +0530

    required all tag from html

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchKeyword = document.getElementById("search");
+const suggestionsContainer = document.getElementById("card-container");
+const favMoviesContainer = document.getElementById("fav-movies-container");
+const emptyText = document.getElementById("empty-search-text");
+const showFavorites = document.getElementById("favorites-section");
+const emptyFavText = document.getElementById("empty-fav-text");
